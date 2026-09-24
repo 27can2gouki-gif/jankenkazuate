225ba90286508ea804f3b41be8da49ab73cc4848
ryota tsuji <ryofu1205@yahoo.co.jp>
2026-09-24 14:29:10 +0900

    ????

--- じゃんけん数当て１/MAIN TITLE.cpp
+++ じゃんけん数当て１/MAIN TITLE.cpp
@@ -1,6 +1,33 @@
 ﻿#include <iostream>
+#include "kazuate.h"
+#include "janken.h"
 using namespace std;
 
-int main(){
-    
+int main() {
+    int GAME = 0;
+    for (; ; ) {
+        cout << "ナニニシマスカ?＞[I]" << endl
+            << endl
+            << "1.ジャンケン" << endl
+            << "2.カズアテ" << endl
+            << "3.システムシュウリョウ" << endl;
+        cin >> GAME;
+        if (GAME == 1) {
+            cout << "ジャンケンゲームヲキドウシマス＞[I]" << janken << flush;
+        }
+        else {
+            if (GAME == 2) {
+                cout << "カズアテゲームヲキドウシマス＞[I]" << kazuate << flush;
+            }
+            else {
+                if (GAME == 3) {
+                    cout << "システムヲシュウリョウシマス＞[I]" << endl;
+                    break;
+                }
+            }
+        }
+        
+
+    }
+    cout << "ゴリヨウアリガトウゴザイマシタ＞[I]ﾉｼ" << endl;
 }
---
+++ じゃんけん数当て１/janken.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int janken() {
+	int answer1 = 0;
+	cout << "ジャンケンゲーム" << endl;
+		for (; ; ) {
+			 cout<< "1.カイシ" << endl
+				<< "2.シュウリョウ" << endl;
+			 cin >> answer1;
+			 if (answer1 == 1) {
+
+			 }
+			return 1;
+		}
+}
